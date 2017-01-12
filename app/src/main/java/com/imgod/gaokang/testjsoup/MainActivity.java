1a9222aefda7f910fb9d61cccde2a11299c90704
gaokang <970720947@qq.com>
2017-01-12 11:42:07 +0800

    edit ui

--- app/src/main/java/com/imgod/gaokang/testjsoup/MainActivity.java
+++ app/src/main/java/com/imgod/gaokang/testjsoup/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -14,10 +15,16 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+/**
+ * 文档地址
+ * http://www.open-open.com/jsoup/
+ */
 public class MainActivity extends AppCompatActivity {
 
     public static final String ImageUrl = "http://www.dbmeinv.com/dbgroup/show.htm?cid=4&pager_offset=1";
-    private TextView txt_main;
+    private Button btn_start;
+    private TextView txt_content;
+    private StringBuilder stringBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initEvent() {
-        txt_main.setOnClickListener(new View.OnClickListener() {
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                stringBuilder = new StringBuilder();
                 startParse();
                 startParseUrl();
             }
@@ -38,23 +46,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        txt_main = (TextView) findViewById(R.id.txt_main);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        txt_content = (TextView) findViewById(R.id.txt_content);
     }
 
     private void startParse() {
         String html = "<html><head><title>First parse</title></head>"
-                + "<body><p>Parsed HTML into a doc.</p></body></html>";
+                + "<body><p>Parsed HTML into a doc.</p><p>Parsed HTML into a doc.1</p><p>Parsed HTML into a doc.2</p></body></html>";
         Document doc = Jsoup.parse(html);
         Elements pelements = doc.select("p");
         for (int i = 0; i < pelements.size(); i++) {
-            Log.e("start", "text:" + pelements.get(0).text());
+            String content = pelements.get(i).text();
+            Log.e("start", "p_" + i + ":" + content);
+            stringBuilder.append("p_" + i + ":" + content + "\n");
         }
+        txt_content.setText(stringBuilder.toString());
         Elements titleelements = doc.select("title");
         for (int i = 0; i < titleelements.size(); i++) {
-            Log.e("start", "text:" + titleelements.get(0).text());
+            String content = titleelements.get(i).text();
+            Log.e("start", "title_" + i + ":" + content);
+            stringBuilder.append("title_" + i + ":" + content + "\n");
         }
-
-        txt_main.setText("title:" + doc.title() + "\nnodeName:" + doc.nodeName());
+        txt_content.setText(stringBuilder.toString());
     }
 
     private void startParseUrl() {
@@ -65,14 +78,22 @@ public class MainActivity extends AppCompatActivity {
                 Document doc = null;
                 try {
                     doc = Jsoup.connect(ImageUrl).timeout(3000).get();
-                    Elements elements = doc.select("div.thumbnail>div.img_single>a");//得到的是每一个图片所指向的连接
-                  //  Elements elements = doc.select("div[class=thumbnail]>div[class=img_single]>a");//得到的是每一个图片所指向的连接
-//                    Elements elements = doc.select("div[class=thumbnail] > div[class=img_single] > a > img");//得到的是每一个图片的地址
+//                    Elements elements = doc.select("div.thumbnail>div.img_single>a");//得到的是每一个图片所指向的连接
+//                      Elements elements = doc.select("div[class=thumbnail]>div[class=img_single]>a");//得到的是每一个图片所指向的连接
+                    Elements elements = doc.select("div[class=thumbnail] > div[class=img_single] > a > img");//得到的是每一个图片的地址
                     final int size = elements.size();
                     for (int i = 0; i < size; i++) {
-                        String imageUrl = elements.get(i).attr("href");//从该节点取出该属性的值
+                        String imageUrl = elements.get(i).attr("src");//从该节点取出该属性的值,前两个Elements拿的是href,第三个取src
                         Log.e("start", "position:" + i + "\timageUrl:\t" + imageUrl);
+                        stringBuilder.append("position:" + i + "\timageUrl:\t" + imageUrl + "\n");
                     }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txt_content.setText(stringBuilder.toString());
+                        }
+                    });
+
                 } catch (IOException e) {
                     Log.e("start", "AsyncTask 遇到异常:" + e.getMessage());
                     e.printStackTrace();
@@ -88,6 +109,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }.execute();
     }
-
-
 }
